573d2ada9c3340faae502ec8330cb28590a072b2
Gautier Colajanni <gautier.colajanni@gmail.com>
2016-05-10 10:51:24 -0400

    Normalize JS files for /p

--- www/p/lib/js/panels/logs_panel.js
+++ www/p/lib/js/panels/logs_panel.js
@@ -1,56 +1,56 @@
 // SETTINGS PANEL
-var output = []
+var output = [];
 var logger = null;
 
-var LogsPanel = function () {
+var LogsPanel = function() {
 	// TODO: move here
 	//this.logger = null;
 	//this.output = [];
 };
 
 LogsPanel.prototype.render = function() {
-	function encode(r){
-		return r.replace(/[\x26\x0A\<>'"]/g,function(r){return"&#"+r.charCodeAt(0)+";"})
+	function encode(r) {
+		return r.replace(/[\x26\x0A\<>'"]/g, function(r) {return '&#' + r.charCodeAt(0) + ';';});
 	}
 	var self = this;
 	$('#logs_tab').html(
-			'<div>'
-			+'<table width="100%"><tr>'
-			+'<td><input class="input" id="logs_nick" style="width:4em" value="user" /></td>'
-			+'<td width="100%"><input class="input" id="logs_input" style="width:100%" /></td>'
-			+'<td><input type="button" class="button" id="logs_button" value="send" style="z-index:-20" /></td>'
-			+'</tr></table>'
-			+'<div id="logs_out" class="consoleout" style="z-index:-20"> </div></div>');
-	r2ui.selected_panel = "Logs";
-	function loginit () {
-		console.log("loginit", logger);
-		var out = document.getElementById ('logs_out');
+			'<div>' +
+			'<table width="100%"><tr>' +
+			'<td><input class="input" id="logs_nick" style="width:4em" value="user" /></td>' +
+			'<td width="100%"><input class="input" id="logs_input" style="width:100%" /></td>' +
+			'<td><input type="button" class="button" id="logs_button" value="send" style="z-index:-20" /></td>' +
+			'</tr></table>' +
+			'<div id="logs_out" class="consoleout" style="z-index:-20"> </div></div>');
+	r2ui.selected_panel = 'Logs';
+	function loginit() {
+		console.log('loginit', logger);
+		var out = document.getElementById('logs_out');
 		if (logger === null) {
-			logger = r2.getTextLogger ();
+			logger = r2.getTextLogger();
 		}
 		logger.render = function() {
 			out.innerHTML = output.join('<br />');
-		}
-		logger.on ('message', function (msg) {
-			output.push (encode (msg.text));
+		};
+		logger.on('message', function(msg) {
+			output.push(encode(msg.text));
 			logger.render();
 		});
 		logger.render();
-		logger.autorefresh (3);
+		logger.autorefresh(3);
 	}
 	function sendmsg() {
-		var nick = document.getElementById ('logs_nick').value.trim();
-		var msg = document.getElementById ('logs_input').value.trim();
+		var nick = document.getElementById('logs_nick').value.trim();
+		var msg = document.getElementById('logs_input').value.trim();
 		//if (!logger) {
-			loginit();
+		loginit();
 		//}
 		if (msg && logger) {
 			if (nick) {
 				msg = '<' + nick + '> ' + msg;
 			}
-			logger.send (msg);
+			logger.send(msg);
 		}
-		document.getElementById ('logs_input').value = '';
+		document.getElementById('logs_input').value = '';
 	}
 	//		loginit();
 	$(document).ready(loginit);
@@ -60,5 +60,5 @@ LogsPanel.prototype.render = function() {
 		}
 	});
 	$('#logs_button').click(sendmsg);
-//	$('#logs_tab').css('color', "rgb(127,127,127);");
+	//	$('#logs_tab').css('color', "rgb(127,127,127);");
 };
--- www/p/lib/js/panels/types_panel.js
+++ www/p/lib/js/panels/types_panel.js
@@ -1,13 +1,13 @@
-var TypesPanel = function () {
+var TypesPanel = function() {
 	this.data = [];
 	this.optionSpacer = 300; //space in px for option buttons
 };
 
 TypesPanel.prototype.insertData = function(k, v, array) {
-	if(typeof array === 'undefined') { array = this.data };
+	if (typeof array === 'undefined') { array = this.data; };
 	var kt = k[0].trim();
 
-	if(k.length == 1) {
+	if (k.length == 1) {
 		//base case
 		array.push({
 			label: kt,
@@ -15,7 +15,7 @@ TypesPanel.prototype.insertData = function(k, v, array) {
 			value: v
 		});
 		return;
-	} else if(array.length < 1) {
+	} else if (array.length < 1) {
 		//if current part of k's path doesn't exist, create it
 		array.push({
 			label: kt ,
@@ -26,15 +26,15 @@ TypesPanel.prototype.insertData = function(k, v, array) {
 	}
 
 	//traverse already populated array to find right spot for insertion
-	for(var i in array) {
-		if(array[i].hasOwnProperty("label") && array[i].label === kt) {
-			if(array[i].hasOwnProperty("children")) {
+	for (var i in array) {
+		if (array[i].hasOwnProperty('label') && array[i].label === kt) {
+			if (array[i].hasOwnProperty('children')) {
 				this.insertData(k.slice(1), v, array[i].children);
 				return;
-			} else if(k.length > 1) {
-				 array[i].children = [];
-				 this.insertData(k.slice(1), v, array[i].children);
-				 return;
+			} else if (k.length > 1) {
+				array[i].children = [];
+				this.insertData(k.slice(1), v, array[i].children);
+				return;
 			}
 		}
 	}
@@ -43,57 +43,57 @@ TypesPanel.prototype.insertData = function(k, v, array) {
 	array.push({
 		label: kt
 	});
-	if(k.length > 1) {
-		array[array.length-1].children = [];
-		this.insertData(k.slice(1), v, array[array.length-1].children);
+	if (k.length > 1) {
+		array[array.length - 1].children = [];
+		this.insertData(k.slice(1), v, array[array.length - 1].children);
 	}
-}
+};
 
 TypesPanel.prototype.generateContent = function() {
 	var ref = this;
-	r2.cmd("tk", function(result) {
-		var strings = result.split("\n");
-		for(var i in strings) {
-			var s = strings[i].split("=");
+	r2.cmd('tk', function(result) {
+		var strings = result.split('\n');
+		for (var i in strings) {
+			var s = strings[i].split('=');
 
-			if(s.length < 2)
+			if (s.length < 2)
 				continue;
-			
-			var k = s[0].split(".");
+
+			var k = s[0].split('.');
 			var v = s[1];
-		
-			if(k.length < 2) {
+
+			if (k.length < 2) {
 				continue;
 			}
 
-			ref.insertData(k, v);					 
+			ref.insertData(k, v);
 		}
-	});	
-}
+	});
+};
 
 TypesPanel.prototype.createBarButtons = function() {
-	var $bar = $("#typesButtonBar");
+	var $bar = $('#typesButtonBar');
 	var $addButton = $('<button id="addButton">Add type</button>');
 	//Can only do files once we can resolve the non-sandboxed path
 	//var $addFileButton = $('<input type="file" style="color: transparent" id="addFileButton"></button>');
 
 	$addButton.click(function() {
-		var str = prompt("Enter C string:");
+		var str = prompt('Enter C string:');
 		r2.cmd('"td ' + str + '"', function() { r2ui._typ.render(); });
 	});
 
-// 	$addFileButton.change(function() {
-// 		var val = $("#addFileButton").val();
-// 		r2.cmd('to ' + val, function() { r2ui._typ.render(); });
-// 	});
+	// 	$addFileButton.change(function() {
+	// 		var val = $("#addFileButton").val();
+	// 		r2.cmd('to ' + val, function() { r2ui._typ.render(); });
+	// 	});
 
 	$bar.append($addButton);
-// 	$bar.append($addFileButton);
+	// 	$bar.append($addFileButton);
 
-}
+};
 
 TypesPanel.prototype.createTree = function() {
-	var $tree = $("#types");
+	var $tree = $('#types');
 	$tree.tree({
 		data: this.data,
 		slide: false,
@@ -101,14 +101,14 @@ TypesPanel.prototype.createTree = function() {
 		useContextMenu: false, //TODO custom context menu for add/remove/edit?
 		selectable: false,
 		onCreateLi: function(node, $li) {
-				var app = "";
-				if(typeof node.value !== 'undefined') {
-					app += " (" + node.value + ")";
+				var app = '';
+				if (typeof node.value !== 'undefined') {
+					app += ' (' + node.value + ')';
 				}
-				if(node.getLevel() == 2) {
+				if (node.getLevel() == 2) {
 					//depth level 2 means we're dealing with an actual type
 					var w = r2ui._typ.optionSpacer;
-					if(node.children && node.children.length != 0) {
+					if (node.children && node.children.length != 0) {
 						w -= 5; //sub 5px to compensate for fold icon
 					}
 
@@ -117,27 +117,27 @@ TypesPanel.prototype.createTree = function() {
 					app +=  '<i class="remove" style="' + style +
 						 '"' + 'data-node-name="' + node.name + '">[-]</i>';
 				}
-				$li.find(".jqtree-element").append(app);
+				$li.find('.jqtree-element').append(app);
 			}
 	});
 
-	$tree.on("click", ".remove", 
+	$tree.on('click', '.remove',
 		function(e) {
 			var label = $(e.target).data('node-name');
-			r2.cmd("t- " + label, function() { r2ui._typ.render(); });
+			r2.cmd('t- ' + label, function() { r2ui._typ.render(); });
 		});
-}
+};
 
 TypesPanel.prototype.render = function() {
-	$("#types_tab").html(
-		'<div id="typesButtonBar"></div>'
-		+ '<div id="types" style="color:rgb(127,127,127)"></div>');
-	
+	$('#types_tab').html(
+		'<div id="typesButtonBar"></div>' +
+		'<div id="types" style="color:rgb(127,127,127)"></div>');
+
 	this.createBarButtons();
 
 	this.data = [];
 	this.maxstr = 0;
 	this.generateContent();
 	this.createTree();
-	
-}
+
+};
