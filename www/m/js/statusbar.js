f4b2f0d6d0abeea84ac8ba39c0c46aca82d14003
Gautier Colajanni <gautier.colajanni@gmail.com>
2016-06-09 13:50:14 -0400

    Fix statusbar behavior (+lint HTML/JS)

--- www/m/js/statusbar.js
+++ www/m/js/statusbar.js
@@ -7,40 +7,43 @@ var statusBig = false;
 var statusTimeout = null;
 
 function statusMessage(x, t) {
-  var statusbar = document.getElementById('statusbar');
-  statusbar.innerHTML = x;
-  if (statusTimeout !== null) {
-    clearTimeout(statusTimeout);
-    statusTimeout = null;
-  }
-  if (t !== undefined) {
-    statusTimeout = setTimeout(function(x) {
-      statusMessage ('');
-    }, t * 1000);
-  }
-  if (x.trim()) {
-    statusLog.push(x);
-  }
+	var statusbar = document.getElementById('statusbar');
+	statusbar.innerHTML = x;
+	if (statusTimeout !== null) {
+		clearTimeout(statusTimeout);
+		statusTimeout = null;
+	}
+	if (t !== undefined) {
+		statusTimeout = setTimeout(function() {
+			statusMessage('');
+		}, t * 1000);
+	}
+	if (x.trim()) {
+		statusLog.push(x);
+	}
 }
 
-function statusToggle(x) {
-  var statusbar = document.getElementById('statusbar');
-  statusBig = !statusBig;
-  if (statusBig) {
-    statusbar.style.height = '50%';
-    statusbar.innerHTML = statusLog.join('<br />');
-  } else {
-    statusbar.style.height = '20px';
-    statusbar.innerHTML = '';
-  }
+function statusToggle() {
+	var statusbar = document.getElementById('statusbar');
+	statusBig = !statusBig;
+	if (statusBig) {
+		statusbar.parentNode.classList.add('bigger');
+		console.log('toggle >big');
+
+		statusbar.innerHTML = statusLog.join('<br />');
+	} else {
+		statusbar.parentNode.classList.remove('bigger');
+		statusbar.innerHTML = '';
+	}
 }
 
 function statusInitialize() {
-  var statusbar = document.getElementById('statusbar');
-  statusbar.addEventListener('click', function() {
-    statusToggle();
-  });
-  statusMessage('Loading webui...', 2);
+	var statusbar = document.getElementById('statusbar');
+	console.log(statusbar.parentNode);
+	statusbar.parentNode.addEventListener('click', function() {
+		statusToggle();
+	});
+	statusMessage('Loading webui...', 2);
 }
 
 statusInitialize();
