c2e7f0019f21791a858b2117e3222de95ab7695a
pancake <pancake@nopcode.org>
2016-06-22 19:16:20 +0200

    Make the /m statusbar more toggleable and fix bottom blankspace

--- www/m/js/statusbar.js
+++ www/m/js/statusbar.js
@@ -3,42 +3,98 @@
  * - support tabs and console
  */
 var statusLog = [];
-var statusBig = false;
+var Mode = {
+	LINE: 0,
+	HALF: 1,
+	FULL: 2
+};
+var statusMode = Mode.LINE;
 var statusTimeout = null;
+
+function getStatusbarBody() {
+	var msg = '<div class>';
+	msg += '<a href="javascript:statusToggle()">[v]</a>&nbsp;';
+	msg += '<a href="javascript:statusFullscreen()">[^]</a>&nbsp;';
+	msg += '<a href="javascript:statusFullscreen()">[$]</a>';
+	msg += '<hr size="1" width="100%" />';
+	msg += '</div>';
+	return msg + statusLog.join('<br />');
+}
 
 function statusMessage(x, t) {
 	var statusbar = document.getElementById('statusbar');
-	statusbar.innerHTML = x;
-	if (statusTimeout !== null) {
-		clearTimeout(statusTimeout);
-		statusTimeout = null;
-	}
-	if (t !== undefined) {
-		statusTimeout = setTimeout(function() {
-			statusMessage('&nbsp;');
-		}, t * 1000);
-	}
-	if (x.trim()) {
+	if (x) {
 		statusLog.push(x);
+	}
+	if (statusMode === Mode.LINE) {
+		statusbar.innerHTML = x;
+		if (statusTimeout !== null) {
+			clearTimeout(statusTimeout);
+			statusTimeout = null;
+		}
+		if (t !== undefined) {
+			statusTimeout = setTimeout(function() {
+				statusMessage('&nbsp;');
+			}, t * 1000);
+		}
+	} else {
+		statusbar.innerHTML = getStatusbarBody();
 	}
 }
 
 function statusToggle() {
 	var statusbar = document.getElementById('statusbar');
-	statusBig = !statusBig;
-	if (statusBig) {
-		statusbar.parentNode.classList.add('bigger');
-		statusbar.innerHTML = statusLog.join('<br />');
-	} else {
-		statusbar.parentNode.classList.remove('bigger');
+	if (statusMode == Mode.HALF) {
+		statusMode = Mode.LINE;
+		statusbar.style.overflow = 'hidden';
 		statusbar.innerHTML = '&nbsp;';
+		try {
+			statusbar.parentNode.classList.remove('half');
+			statusbar.parentNode.classList.remove('full');
+		} catch (e) {
+		}
+	} else {
+		statusbar.innerHTML = getStatusbarBody();
+		statusbar.style.overflow = 'scroll';
+		statusMode = Mode.HALF;
+		try {
+			statusbar.parentNode.classList.remove('full');
+		} catch (e) {
+		}
+		statusbar.parentNode.classList.add('half');
+	}
+}
+
+function statusFullscreen() {
+	var statusbar = document.getElementById('statusbar');
+	if (statusMode == Mode.FULL) {
+		statusbar.style.overflow = 'scroll';
+		statusMode = Mode.HALF;
+		try {
+			statusbar.parentNode.classList.remove('full');
+		} catch (e) {
+		}
+		statusbar.parentNode.classList.add('half');
+	} else {
+		statusMode = Mode.FULL;
+		statusbar.style.overflow = 'scroll';
+		statusbar.innerHTML = getStatusbarBody();
+		try {
+			statusbar.parentNode.classList.remove('half');
+		} catch (e) {
+			/* do nothing */
+		}
+		statusbar.parentNode.classList.add('full');
 	}
 }
 
 function statusInitialize() {
 	var statusbar = document.getElementById('statusbar');
+	statusbar.innerHTML = '';
 	statusbar.parentNode.addEventListener('click', function() {
-		statusToggle();
+		if (statusMode == Mode.LINE) {
+			statusToggle();
+		}
 	});
 	statusMessage('Loading webui...', 2);
 }
