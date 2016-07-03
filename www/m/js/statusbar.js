8bd957617597a8b23bf6e00a9a37c5bc0192a5da
pancake <pancake@nopcode.org>
2016-07-04 01:27:54 +0200

    Add console in statusbar and enhance usability of it

--- www/m/js/statusbar.js
+++ www/m/js/statusbar.js
@@ -8,17 +8,62 @@ var Mode = {
 	HALF: 1,
 	FULL: 2
 };
+var Tab = {
+	LOGS: 0,
+	CONSOLE: 1
+};
 var statusMode = Mode.LINE;
 var statusTimeout = null;
+var statusTab = Tab.LOGS;
 
-function getStatusbarBody() {
-	var msg = '<div>';
-	msg += '<a href="javascript:statusToggle()">[v]</a>&nbsp;';
-	msg += '<a href="javascript:statusFullscreen()">[^]</a>&nbsp;';
-	msg += '<a href="javascript:statusFullscreen()">[$]</a>';
-	msg += '<hr size="1" width="100%" />';
-	msg += '</div>';
-	return msg + statusLog.join('<br />');
+function setStatusbarBody() {
+	function addElement(e, id) {
+		var doc = document.createElement(e);
+		doc.id = id;
+		doc.className = id;
+		return doc;
+	}
+	var doc;
+	try {
+		var statusbar = document.getElementById('tab_terminal');
+		statusbar.innerHTML = '';
+		statusbar.parentNode.removeChild(statusbar);
+	} catch (e) {
+	}
+	try {
+		var statusbar = document.getElementById('tab_logs');
+		statusbar.innerHTML = '';
+		statusbar.parentNode.removeChild(statusbar);
+	} catch (e) {
+	}
+	switch (statusTab) {
+	case Tab.LOGS:
+		var parser = new DOMParser();
+		var doc = document.createElement('div');
+		doc.id = 'tab_logs';
+		var msg = statusLog.join('<br />');
+		doc.appendChild (parser.parseFromString(msg, "text/xml").documentElement);
+		return doc; //break;
+	case Tab.CONSOLE:
+		var doc = document.createElement('div');
+		doc.id = 'tab_terminal';
+		doc.appendChild(addElement('div', 'terminal'));
+		doc.appendChild(addElement('div', 'terminal_output'));
+		var pr0mpt = addElement('div', 'terminal_prompt');
+		pr0mpt.appendChild(addElement('input', 'terminal_input'));
+		doc.appendChild(pr0mpt);
+		break;
+	}
+	if (doc !== undefined) {
+		var statusbar = document.getElementById('statusbar');
+		var terminal = document.getElementById('terminal');
+		if (!terminal) {
+			statusbar.parentNode.insertBefore (doc, statusbar);
+			if (statusTab === Tab.CONSOLE) {
+				terminal_ready ();
+			}
+		}
+	}
 }
 
 function statusMessage(x, t) {
@@ -38,7 +83,7 @@ function statusMessage(x, t) {
 			}, t * 1000);
 		}
 	} else {
-		statusbar.innerHTML = getStatusbarBody();
+		setStatusbarBody();
 	}
 }
 
@@ -46,6 +91,7 @@ function statusToggle() {
 	var statusbar = document.getElementById('statusbar');
 	var container = document.getElementById('container');
 	if (statusMode == Mode.HALF) {
+		statusTab = Tab.LOGS;
 		statusMode = Mode.LINE;
 		statusbar.innerHTML = '&nbsp;';
 		try {
@@ -56,18 +102,28 @@ function statusToggle() {
 			container.classList.remove('sbIsFull');
 		} catch (e) {
 		}
+		setStatusbarBody();
 	} else {
-		statusbar.innerHTML = getStatusbarBody();
 		statusMode = Mode.HALF;
 		try {
 			statusbar.parentNode.classList.remove('full');
 			container.classList.remove('sbIsFull');
-
 		} catch (e) {
 		}
 		statusbar.parentNode.classList.add('half');
 		container.classList.add('sbIsHalf');
+		//setStatusbarBody();
 	}
+}
+
+function statusConsole() {
+	if (statusTab == Tab.CONSOLE) {
+		statusTab = Tab.LOGS;
+
+	} else {
+		statusTab = Tab.CONSOLE;
+	}
+	setStatusbarBody();
 }
 
 function statusFullscreen() {
@@ -84,7 +140,6 @@ function statusFullscreen() {
 		container.classList.add('sbIsHalf');
 	} else {
 		statusMode = Mode.FULL;
-		statusbar.innerHTML = getStatusbarBody();
 		try {
 			statusbar.parentNode.classList.remove('half');
 			container.classList.remove('sbIsHalf');
@@ -96,7 +151,31 @@ function statusFullscreen() {
 	}
 }
 
+
+function addButton(label, callback) {
+	var a = document.createElement('a');
+	a.href = 'javascript:'+callback+'()';
+	a.innerHTML = label;
+	return a;
+}
+
+function initializeStatusbarTitle() {
+	var title = document.getElementById('statusbar_title');
+	var div = document.createElement('div');
+	title.class = 'statusbar_title';
+	title.id = 'statusbar_title';
+	div.className = 'statusbar_title';
+	div.style.textAlign = 'right';
+	div.appendChild (addButton ('v ', 'statusToggle'));
+	div.appendChild (addButton ('^ ', 'statusFullscreen'));
+	div.appendChild (addButton ('$ ', 'statusConsole'));
+	div.appendChild (addButton ('> ', 'statusBarAtRight'));
+	title.parentNode.replaceChild (div, title);
+	// title.parentNode.insertBefore (div, title);
+}
+
 function statusInitialize() {
+	initializeStatusbarTitle();
 	var statusbar = document.getElementById('statusbar');
 	statusbar.innerHTML = '';
 	statusbar.parentNode.addEventListener('click', function() {
@@ -109,3 +188,42 @@ function statusInitialize() {
 
 statusInitialize();
 
+/* --- terminal.js --- */
+function submit(cmd) {
+	var output = document.getElementById('terminal_output');
+	var input = document.getElementById('terminal_input');
+	if (!input || !output) {
+		console.error('No terminal_{input|output} found');
+		return;
+	}
+	if (cmd === 'clear') {
+		output.innerHTML = '';
+		input.value = '';
+		return;
+	}
+	r2.cmd(cmd, function(res) {
+		res += '\n';
+		output.innerHTML += ' > '
+			+ cmd + '\n' + res;
+		input.value = '';
+		var bar = document.getElementById('statusbar_scroll');
+		bar.scrollTop = bar.scrollHeight;
+	});
+}
+
+function terminal_ready() {
+	r2.cmd("e scr.color=true");
+	var input = document.getElementById('terminal_input');
+	if (!input) {
+		console.error('Cannot find terminal_input');
+		return;
+	}
+	input.focus();
+	input.onkeypress = function(e){
+	    if (e.keyCode == 13) {
+		submit(input.value);
+	    }
+	}
+}
+
+/* --- terminal.js --- */
