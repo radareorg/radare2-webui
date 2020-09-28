3e2dbd94c1eff3cf518fc82a58f17404ee5f03f0
pancake <pancake@nopcode.org>
2020-09-29 00:05:00 +0200

    t: fix the gulp build

--- www/t/gulpfile.js
+++ www/t/gulpfile.js
@@ -3,25 +3,24 @@ var gulp = require('gulp'),
 	gzip = require('gulp-gzip'),
 	uglify = require('gulp-uglify'),
 	cleanCSS = require('gulp-clean-css'),
-	concat = require('gulp-concat');
-
-var gulpUtil = require('gulp-util');
+	concat = require('gulp-concat'),
+	gulpUtil = require('gulp-util');
 
 var R2 = '../lib/';
 var DEST = '../../dist/t/'
 
-gulp.task('default', function() {
-	gulp.src(['js/tiled.js', R2+'r2.js', 'js/main.js', 'vendors/material-design-lite/material.min.js'])
+gulp.task('default', async function() {
+	await gulp.src(['js/tiled.js', R2 + 'r2.js', 'js/main.js'])
 		// .pipe(uglify())
 		.pipe(uglify().on('error', gulpUtil.log))
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest(DEST));
 
-	gulp.src(['css/*.css', 'vendors/material-design-lite/material.min.css'])
+	await gulp.src(['css/*.css'])
 		.pipe(cleanCSS())
 		.pipe(concat('stylesheet.css'))
 		.pipe(gulp.dest(DEST));
 
-	gulp.src(['./index.html', './rlogo.png'])
+	return gulp.src(['./index.html', './rlogo.png'])
 		.pipe(gulp.dest(DEST));
 });
