e1922aefd6803e3fc46d67a99313bb57b270b3cc
Georges-B. Michel <georges@reversense.com>
2022-04-16 20:02:35 +0200

    Remove browserify and babel

--- www/m/webpack.config.js
+++ www/m/webpack.config.js
@@ -1,25 +1,41 @@
+// const TerserPlugin = require("terser-webpack-plugin");
+const _path_ = require('path');
+const PROD_MODE = 'production';
+const DEV_MODE = 'development';
+
+const MODE = DEV_MODE;
+
+const EXT_LIBS = './node_modules' ; // ./vendors
+
 module.exports = [{
-  entry: './js/app.js',
+  entry: {
+    main: './js/app.js'
+  },
+  mode: MODE,
   output: {
+    path: _path_.resolve(__dirname, 'dist'),
     // This is necessary for webpack to compile
     // But we never use style-bundle.js
-    filename: 'style-bundle.js',
+    //filename: 'style-bundle.js',
+    filename: 'main.min.js',
   },
+  /*optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },*/
   module: {
     rules: [
       {
+        test: /\.js$/,
+        use: []
+      }/*,
+      {
         test: /\.scss$/,
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'bundle.css',
-            },
-          },
+          { loader: 'file-loader', options: {  name: 'bundle.css' } },
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
+          { loader: 'sass-loader',
             options: {
               // Prefer Dart Sass
               implementation: require('sass'),
@@ -27,9 +43,14 @@ module.exports = [{
               // See https://github.com/webpack-contrib/sass-loader/issues/804
               webpackImporter: false,
             },
-          },
+          }
         ]
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        use: []
+      },*/
     ]
   },
 }];
